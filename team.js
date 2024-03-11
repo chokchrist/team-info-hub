d947ecc3dcfdb5fa34b8b3ffac88ff210f544ebc
Ona Benedi <onabenedi@gmail.com>
2024-03-11 18:25:08 +0100

    push con el objeto ona creado

--- team.js
+++ team.js
@@ -1,1 +1,13 @@
-let team = [];
+let team = [
+   Ona = {
+    surname: "Benedi",
+    age: 23,
+    city: "Sant Esteve d'en Bas",
+    hooby: "reading",
+    favoriteFood: "pancakes",
+    favoriteVideoGame: "Harvest Moon",
+    favoriteFilm: "Shrek 2",
+    favoriteBook: "Looking for Alaska",
+    petName: "Lluna"
+    },
+];
