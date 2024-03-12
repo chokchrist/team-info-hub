0cd477e1c734a489207652fd0841d01b58f021cc
Ona Benedi <onabenedi@gmail.com>
2024-03-12 11:09:28 +0100

    archivo team arreglado :)

--- team.js
+++ team.js
@@ -1,13 +1,1 @@
-let team = [
-   Ona = {
-    surname: "Benedi",
-    age: 23,
-    city: "Sant Esteve d'en Bas",
-    hooby: "reading",
-    favoriteFood: "pancakes",
-    favoriteVideoGame: "Harvest Moon",
-    favoriteFilm: "Shrek 2",
-    favoriteBook: "Looking for Alaska",
-    petName: "Lluna"
-    },
-];
+let team = [];
