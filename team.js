e33ad732b87c658b7b0b417127936507c7315b9d
María Bona <mariabonaenguita@gmail.com>
2024-03-12 10:34:23 +0100

    New info added

---
+++ team.js
@@ -0,0 +1,14 @@
+let team = [
+    {
+      name: "María",
+      surname: "Bona",
+      age: 29,
+      city: "Zaragoza",
+      hobby: "reading",
+      favoriteFood: "ramen",
+      favoriteVideoGame: "Persona 5 Royale",
+      favoriteFilm: "La novia",
+      favoriteBook: "El archivo de las tormentas - Brandon Sanderson",
+      petName: "Zoro",
+    },
+  ];
