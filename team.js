0dab8afd768961ced9d56ce2320f70bb9e9e5ad1
Cinthya <cinthyaredondo12@gmail.com>
2024-03-12 11:26:25 +0100

    I added the exercise

--- team.js
+++ team.js
@@ -1,1 +1,27 @@
-let team = [];
+let team = [
+  {
+    name: "Cinthya",
+    surname: "Redondo Soto",
+    age: 28,
+    city: "Málaga",
+    hobby: "Watching TV series",
+    favoriteFood: "Paella",
+    favoriteVideoGame: "The Sims",
+    favoriteFilm: "Harry Potter and the globet of fire",
+    favoriteBook: "Mistborn Series",
+    petName: "Asta",
+  },
+];
+
+function alphabeticalOrder(surnameName) {
+  surnameName.sort(function (a, b) {
+    return a.surname.localeCompare(b.surname);
+  });
+
+  surnameName.forEach(function (member) {
+    console.log(`${member.surname}, ${member.name}`);
+  });
+
+}
+
+alphabeticalOrder(team);
