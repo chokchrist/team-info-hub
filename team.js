c332c23296a4d84f0c2bdb4516b7720278e8c915
Cinthya <cinthyaredondo12@gmail.com>
2024-03-13 15:05:41 +0100

    I added a new version

--- team.js
+++ team.js
@@ -11,17 +11,45 @@ let team = [
     favoriteBook: "Mistborn Series",
     petName: "Asta",
   },
+  {
+    name: "Cinthya",
+    surname: "álamo Soto",
+    age: 28,
+    city: "Málaga",
+    hobby: "Watching TV series",
+    favoriteFood: "Paella",
+    favoriteVideoGame: "The Sims",
+    favoriteFilm: "Harry Potter and the globet of fire",
+    favoriteBook: "Mistborn Series",
+    petName: "Asta",
+  },
 ];
 
-function alphabeticalOrder(surnameName) {
-  surnameName.sort(function (a, b) {
-    return a.surname.localeCompare(b.surname);
-  });
+function cicleBubbleSort(words) {
+  let change = false;
 
-  surnameName.forEach(function (member) {
-    console.log(`${member.surname}, ${member.name}`);
-  });
+  for (let i = 0; i < words.length - 1; i++) {
+    let comparison = words[i].localeCompare(words[i + 1], "es", {
+      sensitivity: "base",
+    });
+    if (comparison > 0) {
+      let aux = words[i];
+      words[i] = words[i + 1];
+      words[i + 1] = aux;
 
+      change = true;
+    }
+  }
+  return change;
 }
 
-alphabeticalOrder(team);
+function alphabeticalOrder(disorderArray) {
+  let result = cicleBubbleSort(disorderArray);
+
+  while (result == true) {
+    result = cicleBubbleSort(disorderArray);
+  }
+  console.log(disorderArray);
+}
+
+alphabeticalOrder(team)
