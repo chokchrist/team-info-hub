dc4420d19052dca206ce43595b6c95fea863a695
Ona Benedi <onabenedi@gmail.com>
2024-03-13 10:40:15 +0100

    array con el objeto ona creado

--- team.js
+++ team.js
@@ -1,1 +1,22 @@
-let team = [];
+let team = [
+    {name: "ona",
+    surname: "Benedi",
+    age: 23,
+    city: "Sant Esteve d'en Bas",
+    hooby: "reading",
+    favoriteFood: "pancakes",
+    favoriteVideoGame: "Harvest Moon",
+    favoriteFilm: "Shrek 2",
+    favoriteBook: "Looking for Alaska",
+    petName: "Lluna"
+    },
+];
+
+function favGame (){
+    if(team.favoriteVideoGame == "LOL"){
+        console.log(team.name);
+    } else if(team.favoriteVideoGame == "League of Legends"){
+        console.log(team.name);
+    }
+}
+favGame()
