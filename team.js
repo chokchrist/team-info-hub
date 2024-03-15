3fa5cd545efc15b8985af57e2af80d64d4a2ff0f
Ruben Lucena <rubenlucena89@gmail.com>
2024-03-15 12:03:46 +0100

    Added team.js to ruben-team

---
+++ team.js
@@ -0,0 +1,30 @@
+let Ruben = {
+    name: "Ruben",
+    surname: "Lucena",
+    age: 20,
+    city: "Granollers",
+    hobby: "Calistenia",
+    favoriteFood: "Sushi",
+    favoriteVideoGame: "Rainbow Six Siege",
+    favoriteFilm: "Paul",
+    favoriteBook: "Harry Potter",
+    petName: "Darko",
+  };
+  
+  let team = [Ruben,Ruben2]
+  
+  let nombresRepetidos = false;
+  
+  for (let i = 0; i < team.length; i++) {
+      for (let j = i + 1; j < team.length; j++) {
+          if (team[i].name === team[j].name && i!==j) {
+              console.log("El nombre " + team[i].name + " está repetido.");
+              nombresRepetidos = true;
+          }
+      }
+  }
+  
+  if (!nombresRepetidos) {
+      console.log("No hay nombres repetidos.");
+    }
+    
